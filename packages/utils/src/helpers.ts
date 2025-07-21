ade8e1b3fd509b89a53a5efc85f642fe30692f1b
dan13ram <dan13ram@gmail.com>
2025-07-21 23:12:50 +0530

    fix: fixed app build with hoisted wagmi dep

--- packages/utils/src/helpers.ts
+++ packages/utils/src/helpers.ts
@@ -149,8 +149,8 @@ export const walletSnippet = (addr: string | number | undefined, chars: number =
 
   return isAddress(_addr)
     ? _addr.substring(0, chars) +
-    '...' +
-    _addr.substring(_addr.length - chars, _addr.length)
+        '...' +
+        _addr.substring(_addr.length - chars, _addr.length)
     : _addr
 }
 
@@ -163,28 +163,27 @@ export const walletSnippet = (addr: string | number | undefined, chars: number =
 
 export const resolvedPromise = (fn: () => void) => new Promise((resolve) => resolve(fn))
 
-
 export function isEqual(a: any, b: any): boolean {
-  if (a === b) return true;
+  if (a === b) return true
 
   if (typeof a !== 'object' || a === null || typeof b !== 'object' || b === null) {
-    return false;
+    return false
   }
 
-  if (Array.isArray(a) !== Array.isArray(b)) return false;
+  if (Array.isArray(a) !== Array.isArray(b)) return false
 
-  const keysA = Object.keys(a);
-  const keysB = Object.keys(b);
+  const keysA = Object.keys(a)
+  const keysB = Object.keys(b)
 
-  if (keysA.length !== keysB.length) return false;
+  if (keysA.length !== keysB.length) return false
 
   for (const key of keysA) {
     if (!keysB.includes(key) || !isEqual(a[key], b[key])) {
-      return false;
+      return false
     }
   }
 
-  return true;
+  return true
 }
 
 /**
