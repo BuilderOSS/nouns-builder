eb2b648432c47007c0c1ae896762981aeab7464f
dan13ram <dan13ram@gmail.com>
2025-08-29 14:57:15 +0530

    feat: added test script in ui package

---
+++ packages/ui/vitest.config.ts
@@ -0,0 +1,20 @@
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: '.',
+  plugins: [react(), vanillaExtractPlugin()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+  },
+  resolve: {
+    alias: {
+      src: path.resolve('src/'),
+    },
+  },
+})
