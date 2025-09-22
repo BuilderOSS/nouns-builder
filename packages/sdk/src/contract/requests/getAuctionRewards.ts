0d0b2146f8416ad91abcfbdf72afc7bcb11fba32
dan13ram <dan13ram@gmail.com>
2025-09-22 19:11:55 +0530

    feat: add getter for auction rewards

---
+++ packages/sdk/src/contract/requests/getAuctionRewards.ts
@@ -0,0 +1,68 @@
+import { PROTOCOL_REWARDS_MANAGER } from '@buildeross/constants'
+import { AddressType, CHAIN_ID } from '@buildeross/types'
+import { serverConfig } from '@buildeross/utils'
+import { readContract, readContracts } from 'wagmi/actions'
+
+import { auctionAbi, protocolRewardsAbi } from '../abis'
+
+export type GetAuctionRewardsResponse = {
+  builderRewardsBPS: number
+  referralRewardsBPS: number
+  founderRewardsBPS: number
+  founderRewardsRecipient: AddressType
+  founderRewardsBalance: bigint
+}
+
+export const getAuctionRewards = async (
+  chainId: CHAIN_ID,
+  auctionAddress: AddressType,
+): Promise<GetAuctionRewardsResponse> => {
+  const [founderReward, referralRewardsBPS, builderRewardsBPS] = await readContracts(
+    serverConfig,
+    {
+      allowFailure: false,
+      contracts: [
+        {
+          address: auctionAddress,
+          abi: auctionAbi,
+          chainId,
+          functionName: 'founderReward',
+          args: [],
+        },
+        {
+          address: auctionAddress,
+          abi: auctionAbi,
+          chainId,
+          functionName: 'referralRewardsBPS',
+          args: [],
+        },
+        {
+          address: auctionAddress,
+          abi: auctionAbi,
+          chainId,
+          functionName: 'builderRewardsBPS',
+          args: [],
+        },
+      ],
+    },
+  )
+
+  const founderRewardsRecipient = founderReward[0]
+  const founderRewardsBPS = founderReward[1]
+
+  const founderRewardsBalance = await readContract(serverConfig, {
+    address: PROTOCOL_REWARDS_MANAGER,
+    abi: protocolRewardsAbi,
+    chainId,
+    functionName: 'balanceOf',
+    args: [founderRewardsRecipient],
+  })
+
+  return {
+    builderRewardsBPS: builderRewardsBPS,
+    referralRewardsBPS: referralRewardsBPS,
+    founderRewardsBPS: founderRewardsBPS,
+    founderRewardsRecipient: founderRewardsRecipient,
+    founderRewardsBalance: founderRewardsBalance as bigint,
+  }
+}
