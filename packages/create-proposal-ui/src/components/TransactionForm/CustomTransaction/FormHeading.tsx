1c3e8447e1a7078517dfb772e1687951b2fa1d89
dan13ram <dan13ram@gmail.com>
2025-10-15 16:31:56 +0530

    fix: fixed peerDeps

--- packages/create-proposal-ui/src/components/TransactionForm/CustomTransaction/FormHeading.tsx
+++ packages/create-proposal-ui/src/components/TransactionForm/CustomTransaction/FormHeading.tsx
@@ -1,33 +1,25 @@
 import { Box, Flex } from '@buildeross/zord'
-import { motion } from 'framer-motion'
 import React, { memo } from 'react'
 
 import { useCustomTransactionStore } from '../../../stores/useCustomTransactionStore'
-import { transactionFlowHeading, transactionFlowWrapper } from './CustomTransaction.css'
+import { transactionFlowHeading } from './CustomTransaction.css'
 
-const NoMemoisedFormHeading: React.FC<{
+const FormHeadingBase: React.FC<{
   sections: any[]
 }> = ({ sections }) => {
-  const [isOpen] = React.useState(false)
   const { active: activeCustomTransactionSection } = useCustomTransactionStore()
 
   return (
     <Flex direction={'column'} width={'100%'}>
-      <motion.div
-        className={transactionFlowWrapper}
-        initial="initial"
-        animate={isOpen ? 'animate' : 'initial'}
-      >
-        <Flex direction={'column'}>
-          <Box className={transactionFlowHeading}>Add Transaction</Box>
-          <Box>
-            Step {activeCustomTransactionSection + 1} of 6:{' '}
-            {sections[activeCustomTransactionSection].title}
-          </Box>
-        </Flex>
-      </motion.div>
+      <Flex direction={'column'}>
+        <Box className={transactionFlowHeading}>Add Transaction</Box>
+        <Box>
+          Step {activeCustomTransactionSection + 1} of 6:{' '}
+          {sections[activeCustomTransactionSection].title}
+        </Box>
+      </Flex>
     </Flex>
   )
 }
 
-export const FormHeading = memo(NoMemoisedFormHeading)
+export const FormHeading = memo(FormHeadingBase)
