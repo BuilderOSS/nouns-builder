d8dcbeb6a3ccaa6b6de1797f166d7f0dcaaaebf5
dan13ram <dan13ram@gmail.com>
2025-10-25 19:49:24 +0530

    feat: support eas attestation in in-house subgraph

---
+++ apps/subgraph/src/eas.ts
@@ -0,0 +1,104 @@
+import { Address, Bytes } from '@graphprotocol/graph-ts'
+
+import {
+  Attested as AttestedEvent,
+  EAS,
+  Revoked as RevokedEvent,
+} from '../generated/EAS/EAS'
+import { Governor } from '../generated/EAS/Governor'
+import { Treasury } from '../generated/EAS/Treasury'
+import { DAO, Proposal, ProposalUpdate } from '../generated/schema'
+import {
+  decodeEscrowDelegate,
+  decodePropdate,
+  ESCROW_DELEGATE_SCHEMA_UID,
+  PROPDATE_SCHEMA_UID,
+} from './utils/eas'
+
+function getAttestation(address: Address, uid: Bytes): Bytes | null {
+  const eas = EAS.bind(address)
+  const attestation = eas.try_getAttestation(uid)
+  if (!attestation.reverted) {
+    return attestation.value.data
+  }
+  return null
+}
+
+function handlePropdateAttestation(event: AttestedEvent): void {
+  const data = getAttestation(event.address, event.params.uid)
+  if (!data) {
+    return
+  }
+  const propdate = decodePropdate(data)
+  if (!propdate) {
+    return
+  }
+  const dao = DAO.load(event.params.recipient.toHexString())
+  if (!dao) {
+    return
+  }
+  const proposal = Proposal.load(propdate.proposalId.toHexString())
+  if (!proposal) {
+    return
+  }
+  const update = new ProposalUpdate(event.params.uid.toHexString())
+  update.proposal = proposal.id
+  update.transactionHash = event.transaction.hash
+  update.timestamp = event.block.timestamp
+  update.messageType = propdate.messageType
+  update.message = propdate.message
+  update.creator = event.params.attester
+  // is zero hash for original update
+  update.originalUpdate = propdate.originalMessageId.toHexString()
+  update.save()
+}
+
+function handleEscrowDelegateAttestation(event: AttestedEvent): void {
+  const data = getAttestation(event.address, event.params.uid)
+  if (!data) {
+    return
+  }
+  const escrowDelegate = decodeEscrowDelegate(data)
+  if (!escrowDelegate) {
+    return
+  }
+  const treasuryAddress = event.params.recipient
+  const treasury = Treasury.bind(treasuryAddress)
+
+  const ownerCall = treasury.try_owner()
+  if (ownerCall.reverted) {
+    return
+  }
+  const governorAddress = ownerCall.value
+  const governor = Governor.bind(governorAddress)
+
+  const tokenCall = governor.try_token()
+  if (tokenCall.reverted) {
+    return
+  }
+
+  const daoAddress = tokenCall.value
+  const dao = DAO.load(daoAddress.toHexString())
+  if (!dao) {
+    return
+  }
+
+  dao.escrowDelegateAddress = escrowDelegate
+  dao.save()
+}
+
+export function handleAttested(event: AttestedEvent): void {
+  if (event.params.schema == ESCROW_DELEGATE_SCHEMA_UID) {
+    handleEscrowDelegateAttestation(event)
+  } else if (event.params.schema == PROPDATE_SCHEMA_UID) {
+    handlePropdateAttestation(event)
+  }
+}
+
+export function handleRevoked(event: RevokedEvent): void {
+  if (event.params.schema == ESCROW_DELEGATE_SCHEMA_UID) {
+    // TODO: handle escrow delegate revokation
+  } else if (event.params.schema == PROPDATE_SCHEMA_UID) {
+    // TODO: handle propdate revokation
+  }
+}
