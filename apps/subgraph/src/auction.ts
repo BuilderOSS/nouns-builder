a678c5dc74e8a4bc3f25d2ee492f576bd220a102
dan13ram <dan13ram@gmail.com>
2025-10-29 16:55:06 +0530

    feat: added feed events to subgraph

--- apps/subgraph/src/auction.ts
+++ apps/subgraph/src/auction.ts
@@ -1,6 +1,14 @@
 import { dataSource } from '@graphprotocol/graph-ts'
 
-import { Auction, AuctionBid, AuctionConfig, DAO } from '../generated/schema'
+import { 
+  Auction, 
+  AuctionBid, 
+  AuctionConfig, 
+  DAO,
+  AuctionCreatedEvent as AuctionCreatedFeedEvent,
+  AuctionBidPlacedEvent as AuctionBidPlacedFeedEvent,
+  AuctionSettledEvent as AuctionSettledFeedEvent
+} from '../generated/schema'
 import {
   AuctionBid as AuctionBidEvent,
   AuctionCreated as AuctionCreatedEvent,
@@ -31,6 +39,18 @@ export function handleAuctionCreated(event: AuctionCreatedEvent): void {
 
   dao.currentAuction = auction.id
   dao.save()
+  
+  // Create feed event
+  let feedEventId = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  let feedEvent = new AuctionCreatedFeedEvent(feedEventId)
+  feedEvent.type = "AUCTION_CREATED"
+  feedEvent.dao = auction.dao
+  feedEvent.timestamp = event.block.timestamp
+  feedEvent.blockNumber = event.block.number
+  feedEvent.transactionHash = event.transaction.hash
+  feedEvent.actor = event.transaction.from
+  feedEvent.auction = auction.id
+  feedEvent.save()
 }
 
 export function handleAuctionSettled(event: AuctionSettledEvent): void {
@@ -55,6 +75,20 @@ export function handleAuctionSettled(event: AuctionSettledEvent): void {
     }
   }
   dao.save()
+  
+  // Create feed event
+  let feedEventId = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  let feedEvent = new AuctionSettledFeedEvent(feedEventId)
+  feedEvent.type = "AUCTION_SETTLED"
+  feedEvent.dao = auction.dao
+  feedEvent.timestamp = event.block.timestamp
+  feedEvent.blockNumber = event.block.number
+  feedEvent.transactionHash = event.transaction.hash
+  feedEvent.actor = event.transaction.from
+  feedEvent.auction = auction.id
+  feedEvent.winner = auction.winningBid ? AuctionBid.load(auction.winningBid!)!.bidder : event.transaction.from
+  feedEvent.amount = auction.winningBid ? AuctionBid.load(auction.winningBid!)!.amount : event.params.amount
+  feedEvent.save()
 }
 
 export function handleAuctionBid(event: AuctionBidEvent): void {
@@ -82,6 +116,19 @@ export function handleAuctionBid(event: AuctionBidEvent): void {
   auction.extended = event.params.extended
   auction.endTime = event.params.endTime
   auction.save()
+  
+  // Create feed event
+  let feedEventId = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  let feedEvent = new AuctionBidPlacedFeedEvent(feedEventId)
+  feedEvent.type = "AUCTION_BID_PLACED"
+  feedEvent.dao = auction.dao
+  feedEvent.timestamp = event.block.timestamp
+  feedEvent.blockNumber = event.block.number
+  feedEvent.transactionHash = event.transaction.hash
+  feedEvent.actor = bid.bidder
+  feedEvent.auction = bid.auction
+  feedEvent.bid = bid.id
+  feedEvent.save()
 }
 
 export function handleDurationUpdated(event: DurationUpdatedEvent): void {
