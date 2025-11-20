6f119b25b4f43b592b97e4a79912bfb668f2838d
dan13ram <dan13ram@gmail.com>
2025-11-20 18:17:06 +0530

    feat: add historical bids to place bid modal

--- packages/feed-ui/src/Modals/BidModal.tsx
+++ packages/feed-ui/src/Modals/BidModal.tsx
@@ -1,9 +1,16 @@
-import { PlaceBid } from '@buildeross/auction-ui'
+import { CurrentAuction } from '@buildeross/auction-ui'
 import { PUBLIC_ALL_CHAINS } from '@buildeross/constants'
-import type { CHAIN_ID, RequiredDaoContractAddresses } from '@buildeross/types'
+import { SWR_KEYS } from '@buildeross/constants/swrKeys'
+import { getBids } from '@buildeross/sdk/subgraph'
+import type {
+  AddressType,
+  CHAIN_ID,
+  RequiredDaoContractAddresses,
+} from '@buildeross/types'
 import { AnimatedModal, SuccessModalContent } from '@buildeross/ui/Modal'
 import { Stack, Text } from '@buildeross/zord'
 import React, { useState } from 'react'
+import useSWR from 'swr'
 
 export interface BidModalProps {
   isOpen: boolean
@@ -13,6 +20,9 @@ export interface BidModalProps {
   daoName: string
   addresses: RequiredDaoContractAddresses
   highestBid?: bigint
+  highestBidder?: AddressType
+  endTime?: number
+  paused?: boolean
 }
 
 export const BidModal: React.FC<BidModalProps> = ({
@@ -23,6 +33,9 @@ export const BidModal: React.FC<BidModalProps> = ({
   daoName,
   addresses,
   highestBid,
+  paused,
+  highestBidder,
+  endTime,
 }) => {
   const [isSuccess, setIsSuccess] = useState(false)
 
@@ -40,6 +53,18 @@ export const BidModal: React.FC<BidModalProps> = ({
     onClose()
   }
 
+  const { data: bids } = useSWR(
+    chainId && tokenId && addresses.token
+      ? ([
+          SWR_KEYS.AUCTION_BIDS,
+          chainId,
+          addresses.token.toLowerCase(),
+          tokenId.toString(),
+        ] as const)
+      : null,
+    ([, _chainId, _collection, _tokenId]) => getBids(_chainId, _collection, _tokenId)
+  )
+
   const chain = PUBLIC_ALL_CHAINS.find((c) => c.id === chainId)!
 
   return (
@@ -55,12 +80,18 @@ export const BidModal: React.FC<BidModalProps> = ({
           ) : (
             <Stack gap="x6" p="x6" w="100%">
               <Text variant="heading-md">Place Your Bid</Text>
-              <PlaceBid
-                chain={chain}
+
+              <CurrentAuction
+                chainId={chain.id}
                 tokenId={tokenId}
+                auctionAddress={addresses.auction}
+                tokenAddress={addresses.token}
+                auctionPaused={paused ?? false}
                 daoName={daoName}
-                addresses={addresses}
-                highestBid={highestBid}
+                bid={highestBid}
+                owner={highestBidder}
+                endTime={endTime}
+                bids={bids || []}
                 onSuccess={handleSuccess}
               />
             </Stack>
